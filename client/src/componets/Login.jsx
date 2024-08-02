2972674bb6022fe6cd1a800a01a342660ed7fbd5
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-02 20:16:24 +0530

    Project Init

---
+++ client/src/componets/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="w-full h-screen bg-gradient-to-l from-sky-500 to-indigo-400">
+      <i className="bi bi-arrow-left-circle-fill"></i>
+      {/* nav */}
+      <nav className="flex justify-between items-center py-4"></nav>
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,3 @@
+import Login from "../componets/Login";
+
+Login;
